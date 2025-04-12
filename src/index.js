7146f9d9c79da089c591190fe24119aad9d0352d
Diditsb <diditsb007@gmail.com>
2025-04-12 23:34:08 +0700

    first code

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import '../src/dist/output.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
